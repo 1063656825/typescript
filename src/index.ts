94803eafcb95e6db47ece40101997da60f0ff49d
1063656825 <1063656825@qq.com>
2021-03-09 16:57:23 +0800

    2-1基本类型

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-let say:String = 'hello';
-console.log('adfaa1');
+let name:string = 'hello';//这个是字符串类型 应该是小写
+
+function sum(a:number,b:number):number{//括号内的是参数 类型写到参数后，在括号外的为函数的返回值
+    return a + b;
+}
+let num = sum(1,2);//在ts中及时不填写类型也会自动推导类型
+let phone = "15841111111"
+let aaa;//这样就是推导不出来的
