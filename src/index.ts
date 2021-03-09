49e8c3007aaf20242eef8a19bec60eeb4ab8c7cc
1063656825 <1063656825@qq.com>
2021-03-09 22:26:50 +0800

    2-3其他类型

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,32 @@
-let name:string = 'hello';//这个是字符串类型 应该是小写
+let num:string | undefined;
 
-function sum(a:number,b:number):number{//括号内的是参数 类型写到参数后，在括号外的为函数的返回值
-    return a + b;
+if(typeof(num) === "string"){
+    // 这里可以使用字符串的方法
 }
-let num = sum(1,2);//在ts中及时不填写类型也会自动推导类型
-let phone = "15841111111"
-let aaa;//这样就是推导不出来的
+
+function printMenu():void{
+    console.log("1.登录");
+    console.log("2.注册");  
+}
+// 这里及时不声明也会推导出来
+
+function throwError(msg:string):never{
+    throw new Error(msg);
+}
+
+let a:"A";//字面量类型   这里常用的就是性别
+let gender:"男"|"女";
+
+let arr:[];//这也是一个字面量类型，就是只能约束为一个空数组；
+
+let user:{
+    name:string
+    age:number
+}//这也是字面量赋值，这里主要是用来固定一个对象
+
+let people:[string,number];//这就是一个元祖，规定数组只能是两项，第一项为字符串，第二项为数字
+
+let data:any;  //any类型可以复制任意类型
+data=1;
+data="2";
+data=[1,2]
