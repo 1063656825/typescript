83b2370eb723035417ba89e61f14b5a33c52c99c
1063656825 <1063656825@qq.com>
2021-03-09 23:05:18 +0800

    2-5函数相关约束

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,56 @@
-// 不用别名的情况
-// let u: {
-//     name: string
-//     age: number
-//     gender: "男" | "女"
+
+// 在这种情况下函数返回值就会出现混乱，还是不便于以后的使用
+// function combine(a:number|string,b:string|number):number|string{
+// if(typeof(a)==="number" && typeof(b)==="number"){
+//     return a * b;
+// } else if (typeof a === "string" && typeof b === "string"){
+//     return a + b;
 // }
-
-// function getUser(): {
-//     name: string
-//     age: number
-//     gender: "男" | "女"
-// }[] {
-//     return []
+// throw new Error("a和b必须是相同的类型")
 // }
+// const result = combine(1,2)
 
-
-// 使用了别名的情况 这样就会避免代码冗余 这里还可以多个别名一起混用
-type Gender = "男" | "女";
-type User = {
-    name: string
-    age: number
-    gender: Gender
+// 这里就是通过函数重载的方式来实现的
+/**
+ *得到a*b的结果
+ * @param {number} a
+ * @param {number} b
+ * @return {*}  {number}
+ */
+function combine(a:number,b:number):number;
+/**
+ *得到a,b两个字符串的拼接结果
+ * @param {string} a
+ * @param {string} b
+ * @return {*}  {string}
+ */
+function combine(a:string,b:string):string;
+function combine(a: number | string, b: string | number): number | string {
+    if (typeof (a) === "number" && typeof (b) === "number") {
+        return a * b;
+    } else if (typeof a === "string" && typeof b === "string") {
+        return a + b;
+    }
+    throw new Error("a和b必须是相同的类型")
 }
+const result = combine("1", "1")
 
-let u: User
 
-function getUser(g:Gender): User[] {
-    return []
+
+/**
+ *这里参数后加？就表示可选参数可以传也可以不传 当后面添加等于号就是添加了默认值  默认参数就是可选参数
+ * @param {number} a
+ * @param {number} b
+ * @param {number} [c]
+ * @return {*} 
+ */
+function sum(a:number,b:number = 10,c?:number){
+    if(c){
+        return a + b + c
+    }else{
+        return a + b
+    }
 }
-
-
-
+sum(1,2,3);
+sum(1,2);
+sum(1)
