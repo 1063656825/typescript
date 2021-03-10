21993e075dcde47cd01c2767d7db62b37735c88d
1063656825 <1063656825@qq.com>
2021-03-10 16:04:19 +0800

    5-1模块化初试

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,6 @@
-enum Permission{
-    Read = 1,    //0001
-    Write = 2,   //0010
-    Create = 4,  //0100
-    Delete = 8,  //1000
-}
+import { name , sum } from "./myModule"
 
-// 如何组合权限
-// 这里使用的是 或 运算
-let p = Permission.Read | Permission.Write;
-p = p | Permission.Delete;
+console.log(name);
 
-// 2. 如何判断是否拥有某个权限
-// 这里使用的是 且 运算
-function hasPermission(target:Permission,per:Permission){
-    return (target & per) === per;
-}
+console.log(sum(1,2));
 
-let result = hasPermission(p, Permission.Delete)
-console.log(result);
-
-// 3. 删除某个权限
-// 这里使用的是 异或 运算   相同取 0 不同取 1
-p = p ^ Permission.Delete;
-
-result = hasPermission(p, Permission.Delete)
-console.log(result);
---
+++ src/myModule.ts
@@ -0,0 +1,5 @@
+export let name = "ads";
+
+export function sum(a:number,b:number){
+    return a + b
+}
