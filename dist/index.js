90c069d4a325be45a8058ad54de7885823496f5b
1063656825 <1063656825@qq.com>
2021-03-10 11:01:22 +0800

    4-1枚举

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,14 @@
-let say = 'hello';
-console.log('aaaa');
+var Gender;
+(function(Gender) {
+    Gender["male"] = "\u7537";
+    Gender["female"] = "\u5973";
+})(Gender || (Gender = {}));
+var level;
+(function(level) {
+    level[level["level1"] = 1] = "level1";
+    level[level["level2"] = 2] = "level2";
+})(level || (level = {}));
+console.log(level);
+let gender;
+gender = Gender.male;
+console.log(gender);
--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,13 @@
-
-type Deck = NormalCard[]
-type Color = "♥" | "♠" | "♣" | "♦"
-type NormalCard = {
-    color: Color
-    mark:number
+enum Gender{
+    male = "男",
+    female = "女"
 }
 
-function createDeck():Deck{
-    let deck:Deck = [];
-    for (let i = 1; i <= 13; i++) {
-        deck.push({
-            color:"♠",
-            mark:i
-        })
-        deck.push({
-            color: "♣",
-            mark: i
-        })
-        deck.push({
-            color: "♥",
-            mark: i
-        })
-        deck.push({
-            color: "♦",
-            mark: i
-        })
-        
-    }
-    return deck
+enum level{
+    level1=1,
+    level2=2
 }
 
-function printDeck(deck:Deck){
-    for (let item of deck) {
-        let str = item.color;
-        if(item.mark == 1){
-            str += 'A'
-        }else if(item.mark <=10){
-            str += item.mark
-        }else if(item.mark == 11){
-            str+="J"
-        } else if (item.mark == 12) {
-            str += "Q"
-        } else if (item.mark == 13) {
-            str += "K"
-        }
-        console.log("这是一张"+str);
-    }
-}
-let result = createDeck();
-printDeck(result)
-function findCardsByColor(color:Color){
-    
-}
+let gender:Gender;
+gender=Gender.male;
+console.log(gender);
