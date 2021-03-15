f0fa02a9177ed368bc66b3b5d368857d1c9e5167
1063656825 <1063656825@qq.com>
2021-03-15 16:10:06 +0800

    8-2多泛型

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,57 @@
-
-class User{
-    readonly id: number
-    // name : string
-    // age : number
-    gender: "男" | "女" = "男" //这是给它添加一个默认值
-    phone?:number
-
-    private _allText: number = 3;//限制的最大文章数
-    private _nowText: number = 0;//当前的文章数
-
-    // 访问器一般使用的是私有属性，私有属性一般还都是 _名 这样就避免的重名
-    constructor(public name:string,private _age:number){//直接使用public 进行修饰就可以进行简写
-        this.id = Math.random()
-        // this.age = age;
-        // this.name = name;
-    }
-
-    // 访问器
-    set age(value: number){
-        if(value < 0){
-            this._age = 0;
-        }else if(value > 200){
-            this._age == 200;
-        }else{
-            this._age = value;
-        }
-    }
-
-    get age(){
-        return this._age;
-    }
-    
-
-    publish(title:string){
-        if(this._nowText < this._allText){
-            console.log("发布一篇文章" + title);
-            this._nowText += 1;
-        }else{
-            console.log("今天已达发布上限");
-            
-        }
-    }
+interface hasNameProperty {
+    name: string
 }
 
-const u = new User("aa",2)
-u.age = 12;
-console.log(u.age);
-u.publish("文章")
-u.publish("文章")
-u.publish("文章")
-u.publish("文章")
-u.publish("文章")
+/**
+ *该方法是将对象中name属性的每个单词首字母大写
+ *
+ * @template T
+ * @param {T} obj
+ * @return {*}  {T}
+ */
+function nameToUpperCase<T extends hasNameProperty>(obj: T): T {
+    obj.name = obj.name
+        .split(" ")
+        .map(item => {
+            item = item[0].toLocaleUpperCase() + item.substr(1);
+            return item;
+        })
+        .join(" ")
+    return obj
+}
+
+
+let peo = {
+    name: "yu tai qi",
+    age: 22,
+    gender: "男"
+}
+
+
+console.log(nameToUpperCase(peo));
+
+/**
+ *将两个数组混合 
+ * @template T
+ * @template K
+ * @param {T[]} arr1
+ * @param {K[]} arr2
+ * @return {*}  {((T | K)[])}
+ */
+function mixinArray<T, K>(arr1: T[], arr2: K[]): (T | K)[] {
+    let newArr: (T | K)[] = [];
+    let num = arr1.length > arr2.length ? arr1.length : arr2.length;
+    for (let i = 0; i < num; i++) {
+        if (arr1.length > i) {
+            newArr.push(arr1[i])
+        }
+        if (arr2.length > i) {
+            newArr.push(arr2[i])
+        }
+    }
+    return newArr
+}
+
+let result = mixinArray([1,2,3,4,5,6,7],["a","b","c","d","e"])
+console.log(result);
+
